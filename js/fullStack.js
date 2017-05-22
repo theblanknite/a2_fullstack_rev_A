e57eea0356f2c0c8869d317535c326cb26d4b1be
theblanknite <steelhooks123484@gmail.com>
2017-05-22 09:42:46 -0600

    Add files via upload

---
+++ js/fullStack.js
@@ -0,0 +1,17 @@
+function showTasks(){
+alert("" +
+"Use Google Fonts, " +
+"Use a good, relevant background picture, \n"+
+"Link to more info, \n" +
+"In an interesting way explain 'Full Stack', \n"+	
+"Make in-line links to the key words used in these explanations, \n" +
+"An interesting way to explain LAMP Stack, \n" +	
+"Explain Database in general using both text and illustration, \n"+ 
+"Explain mySQL with illustration, \n" +
+"Explain SQL, \n" +
+"Explain PHP, \n" +
+"Illustrate a DB Table, \n" +
+"List and explain mysql CLI steps to create a DB, table, records");	
+}
+
+
